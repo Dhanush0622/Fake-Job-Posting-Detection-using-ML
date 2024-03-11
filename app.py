9bc0523e1232da9da06cc52235000c02113e58fa
Dhanush6022 <dhanush6022@gmail.com>
2024-03-11 14:33:46 +0530

    Commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5bd7c4af3631592cb9167249d5ec6d760f401bb97ff4dbfa72d0c99c4ad9057a
+size 864
---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:420e413d7a90d5dcb7585c7bfbae7053b74e5174c4f365a6e6799a63591c8b14
+size 3844
